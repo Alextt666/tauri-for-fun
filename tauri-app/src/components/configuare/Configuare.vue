e5eb9e6d8a9a274f57c41816706b2fab28e81029
alexzhang <alexzhangyaotian@gmail.com>
2024-04-01 14:04:41 +0800

    feat:网域设置

---
+++ tauri-app/src/components/configuare/Configuare.vue
@@ -0,0 +1,33 @@
+<template>
+  <div className="cursor-pointer pr-10 pt-5" @click="handleConfiguare">❀</div>
+  <el-dialog v-model="configVisible">
+    <div className="font-bold mb-5 text-center text-lg">网域选择</div>
+    <template #footer>
+      <div className="flex">
+        <el-radio-group v-model="netOption" className="ml-4">
+          <el-radio value="local" size="large">局域网</el-radio>
+          <el-radio value="remote" size="large">公网</el-radio>
+        </el-radio-group>
+      </div>
+
+      <div class="dialog-footer">
+        <el-button @click="handleCancel">取消</el-button>
+        <el-button type="primary" @click="handleConfirm"> 确认 </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+<script setup>
+import { ref } from "vue";
+const configVisible = ref(false);
+const netOption = ref("local");
+const handleConfiguare = () => {
+  configVisible.value = true;
+};
+const handleCancel = () => {
+  configVisible.value = false;
+};
+const handleConfirm = () => {
+  configVisible.value = false;
+};
+</script>
--- tauri-app/src/layout/Navbar.vue
+++ tauri-app/src/layout/Navbar.vue
@@ -2,9 +2,11 @@
   <div className="w-full flex justify-end">
     <Setting />
     <Explayer />
+    <Configuare />
   </div>
 </template>
 <script setup>
 import Setting from "@/components/settings/index.vue";
 import Explayer from "@/components/explayer/Explayer.vue";
+import Configuare from "@/components/configuare/Configuare.vue";
 </script>
